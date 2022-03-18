708359a4fb9eb89db9d57816984ba7639be3b9c9
shana <randomshana1@gmail.com>
2022-03-18 18:31:37 +0530

    added bool function

---
+++ boolFun.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract boolFun{
+    function isBool(int num) public pure returns(bool){
+        bool flag = true;
+        if (num<0){
+            flag = false;
+        }
+        return flag;
+    }
+}
