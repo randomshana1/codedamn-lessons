82087a989a74c441f5c679c962d47747e25b4864
shana <randomshana1@gmail.com>
2022-03-21 21:06:39 +0530

    added struct and enum codes

---
+++ loops.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract loops {
+    uint[] public arr = [10,20,30,40,50 ];
+
+    function createLoop() public {
+        for (uint i = 0 ; i < arr.length ; i++){
+            arr[i] = 2 * arr[i];
+        }
+    }
+
+    function returnArray() public view returns(uint[] memory){
+        return arr;
+    }
+}
