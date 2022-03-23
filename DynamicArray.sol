b6cb5702658179364aa93f87ac30f5f1e07c77f4
shana <randomshana1@gmail.com>
2022-03-23 16:10:30 +0530

    updated

--- DynamicArray.sol
+++ DynamicArray.sol
@@ -4,16 +4,11 @@ pragma solidity >=0.5.0 < 0.9.0;
 contract DynamicArray {
     uint[] public id;
 
-    function insert(uint item) public {
+    function Insert(uint item) public {
         id.push(item);
     } 
 
-    function length() public view returns(uint){
-        return id.length;
-    }
-
-    function get( uint item) public view returns(uint){
-
+    function get(uint item) public view returns(uint){
         for (uint i=0; i<id.length;i++){
             if (id[i] == item ){
                 return i;
@@ -23,5 +18,43 @@ contract DynamicArray {
 
     function getAll()public view returns(uint[] memory){
         return id;
+    }
+
+    function length() public view returns(uint){
+        return id.length;
+    }
+}
+
+
+
+
+
+-------
+
+
+// SPDX-License-Identifier: MIT 
+pragma solidity >=0.5.0 < 0.9.0;
+
+contract DynamicArray {
+    uint256[] public id;
+
+    function Insert(uint256 num) public {
+        id.push(num);
+    }
+
+    function get(uint256 num) public view returns (uint256) {
+        for (uint256 i = 0; i < id.length; i++) {
+            if (num == id[i]) {
+                return i;
+            }
+        }
+    }
+
+    function getAll() public view returns (uint256[] memory) {
+        return id;
+    }
+
+    function length() public view returns (uint256) {
+        return id.length;
     }
 }
--- parentAb.sol
+++ parentAb.sol
@@ -18,7 +18,7 @@ pragma solidity >=0.5.0 < 0.9.0;
 //     // }
 // }
 
-// // SPDX-License-Identifier: MIT
+
 // pragma solidity >=0.5.0 < 0.9.0;
 
 
