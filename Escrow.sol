f6ff563ddddcc1a93b1f043aa44b3265d3e362ac
shana <randomshana1@gmail.com>
2022-03-25 14:58:57 +0530

    added escrow

---
+++ Escrow.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Escrow {
+
+    address payable public payer;
+    address payable public payee;
+    address payable public thirdParty;
+    uint256 public amount;
+
+    constructor(address payable _payer, address payable _payee, uint256 _amount){
+
+        payer = _payer;
+        payee = _payee;
+        amount = _amount;
+        thirdParty = payable (msg.sender);
+
+    }
+
+    function deposit() public view {
+
+        require(payer == msg.sender,"Sender must be the payer");
+        require(address(this).balance >= amount,"Cant send more than escrow amount");
+
+    }
+
+    function release() public {
+
+        require(address(this).balance == amount, "cannot release funds before full amount is sent");
+        require(msg.sender == thirdParty,"only thirdParty can release funds");
+        payee.transfer(amount);
+    }
+
+    function balanceOf() public view returns(uint256){
+
+        return address(this).balance;
+
+    }
+
+
+}
