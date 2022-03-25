bf581efc9e0ec2f1921646ddcee3951bf8b7404f
randomshana1 <101309741+randomshana1@users.noreply.github.com>
2022-03-25 09:55:08 +0530

    Update transferEtherContract.sol

---
+++ transferEtherContract.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract transferEtherContract{
+    address payable user = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+
+    function payEther() payable public {
+
+    }
+
+
+    function checkBalance() view public returns(uint){
+        return address(this).balance;
+    }
+
+    function trasferEther(uint amount) public {
+        user.transfer(amount);
+    }
+}
